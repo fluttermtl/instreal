d7ca38109c7b521715a7b20be68303ac68a3c1ad
Ali Yazdi <ma.yazdi75@gmail.com>
2023-06-14 16:28:13 -0400

    change to m3 and post view (#21)

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:instreal/l10n/index.dart';
-import 'package:instreal/presentation/themes/instreal_theme_data.dart';
+import 'package:instreal/presentation/screens/home/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,48 +18,23 @@ class MyApp extends StatelessWidget {
       supportedLocales: InstrealLocalizations.supportedLocales,
       debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.dark,
-      theme: InstrealThemeData.lightThemeData,
-      darkTheme: InstrealThemeData.darkThemeData,
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(32.0),
-        child: ListView(
-          children: [
-            Text(
-              InstrealLocalizations.of(context)!.whatsUpIn,
-              style: Theme.of(context).textTheme.titleLarge,
-            ),
-            Text(
-              InstrealLocalizations.of(context)!.montreal,
-              style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                    fontFamily: GoogleFonts.caveat().fontFamily,
-                    color: Theme.of(context).colorScheme.primary,
-                  ),
-            ),
-            const Card(
-              color: Colors.white,
-              child: Text('Test the text'),
-            ),
-          ],
-        ),
+      theme: ThemeData(
+        useMaterial3: true,
+        brightness: Brightness.light,
+        colorSchemeSeed: Colors.purple,
+        textTheme: ThemeData.light()
+            .textTheme
+            .apply(fontFamily: GoogleFonts.openSans().fontFamily),
       ),
+      darkTheme: ThemeData(
+        useMaterial3: true,
+        brightness: Brightness.dark,
+        colorSchemeSeed: Colors.purple,
+        textTheme: ThemeData.dark()
+            .textTheme
+            .apply(fontFamily: GoogleFonts.openSans().fontFamily),
+      ),
+      home: const MyHomePage(),
     );
   }
 }
